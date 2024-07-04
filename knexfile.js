25d45f360e2b16d9a433e7208de436c1300253bd
AsegawaShiyu <84178195+AsegawaShiyu@users.noreply.github.com>
2024-07-04 06:43:15 +0000

    13

--- knexfile.js
+++ knexfile.js
@@ -6,62 +6,41 @@
 module.exports = {
 
   development: {
-    client: 'pg',
+    client: 'sqlite3',
     connection: {
-      database: 'todo_app',
-      user:     'root',
-      password: 'postgres'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      directory: './db/migrations',
-      tableName: 'knex_migrations'
-    },
-    seeds: {
-      directory: './db/seeds',
+      filename: './dev.sqlite3'
     }
   },
 
   staging: {
-    client: 'pg',
+    client: 'postgresql',
     connection: {
-      database: 'todo_app',
-      user:     'root',
-      password: 'postgres'
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
     },
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      directory: './db/migrations',
       tableName: 'knex_migrations'
-    },
-    seeds: {
-      directory: './db/seeds',
     }
   },
 
   production: {
-    client: 'pg',
+    client: 'postgresql',
     connection: {
-      database: 'todo_app',
-      user:     'root',
-      password: 'postgres'
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
     },
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      directory: './db/migrations',
       tableName: 'knex_migrations'
-    },
-    seeds: {
-      directory: './db/seeds',
     }
   }
 
