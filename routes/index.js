70a3c8bf27226499f3b3ba345dbfc690a922cdf4
AsegawaShiyu <84178195+AsegawaShiyu@users.noreply.github.com>
2024-08-08 13:12:52 +0000

    ohayou

--- routes/index.js
+++ routes/index.js
@@ -3,27 +3,41 @@ const router = express.Router();
 const knex = require('../db/knex');
 
 router.get('/', function (req, res, next) {
-  knex("tasks")
-    .select("*")
-    .then(function (results) {
-      console.log(results);
-      res.render('index', {
-        title: 'ToDo App',
-        todos: results,
+  const isAuth = req.isAuthenticated();
+  if (isAuth) {
+    const userId = req.user.id;
+    knex("tasks")
+      .select("*")
+      .where({user_id: userId})
+      .then(function (results) {
+        res.render('index', {
+          title: 'ToDo App',
+          todos: results,
+          isAuth: isAuth,
+        });
+      })
+      .catch(function (err) {
+        console.error(err);
+        res.render('index', {
+          title: 'ToDo App',
+          isAuth: isAuth,
+          errorMessage: [err.sqlMessage],
+        });
       });
-    })
-    .catch(function (err) {
-      console.error(err);
-      res.render('index', {
-        title: 'ToDo App',
-      });
+  } else {
+    res.render('index', {
+      title: 'ToDo App',
+      isAuth: isAuth,
     });
+  }
 });
 
 router.post('/', function (req, res, next) {
+  const isAuth = req.isAuthenticated();
+  const userId = req.user.id;
   const todo = req.body.add;
   knex("tasks")
-    .insert({user_id: 1, content: todo})
+    .insert({user_id: userId, content: todo})
     .then(function () {
       res.redirect('/')
     })
@@ -31,10 +45,14 @@ router.post('/', function (req, res, next) {
       console.error(err);
       res.render('index', {
         title: 'ToDo App',
+        isAuth: isAuth,
+        errorMessage: [err.sqlMessage],
       });
     });
 });
 
 router.use('/signup', require('./signup'));
+router.use('/signin', require('./signin'));
+router.use('/logout', require('./logout'));
 
 module.exports = router;
